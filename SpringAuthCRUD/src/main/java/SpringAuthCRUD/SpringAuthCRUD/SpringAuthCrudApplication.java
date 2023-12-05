dfa7e7be4b14ad80fcffb9b48783dfb435b51758
mohamed khalil bchir <101249582+Khalil-Bchir@users.noreply.github.com>
2023-12-05 13:58:34 +0100

    Spring projecte init

---
+++ SpringAuthCRUD/src/main/java/SpringAuthCRUD/SpringAuthCRUD/SpringAuthCrudApplication.java
@@ -0,0 +1,13 @@
+package SpringAuthCRUD.SpringAuthCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthCrudApplication.class, args);
+	}
+
+}
---
+++ SpringAuthCRUD/src/test/java/SpringAuthCRUD/SpringAuthCRUD/SpringAuthCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringAuthCRUD.SpringAuthCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
